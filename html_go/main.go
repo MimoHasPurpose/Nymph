de471b68566b4d6775c40dab2b79c249d429480f
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-06-06 06:58:41 +0530

    added html/css and js func

--- html_go/main.go
+++ html_go/main.go
@@ -1,8 +1,10 @@
 package main
-
 import (
   
     "os"
+	"math/rand/v2"
+	"strconv"
+	"fmt"
 )
 
 func check(e error) {
@@ -10,22 +12,37 @@ func check(e error) {
         panic(e)
     }
 }
-func project(project_name){
-	err:=os.Mkdir("${project_name}",0755)
-	check(err)
+func createfile(){
+	a:=rand.IntN(100)
+    ht := []byte("<head>\n<title></title>\n</head>\n<body>\n</body>")
+	cs := []byte("*{background-color: black; size:20px;}")
+	js := []byte("console.log(\"hello world)")
+    t:=strconv.Itoa(a)
+	fmt.Printf("what file do u wanna create!\n 1. html \n2.css \n3.javascript\n  ")
+	var i int
+	fmt.Scan(&i)
+	switch i {
+    case 1:
+		err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".html", ht, 0644)
+    	check(err)
+    case 2:
+        err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".css", cs, 0644)
+    	check(err)
+    case 3:
+        err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".js", js, 0644)
+    	check(err)
+	case 4:
+		err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".html", ht, 0644)
+		err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".css", cs, 0644)
+		err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\"+"file"+t+".js", js, 0644)
+		check(err)
+    }
 }
-func file(file_name){
-	create:=func(file_name string){
-		d:=[]byte("")
-		check(os.WriteFile(file_name,d,0644))
-	}
-	create("{project_name}/{file_name}")
-}
+
 func main() {
 	
-    d1 := []byte("<head>\n<title></title>\n</head>\n<body>\n</body>")
-    err := os.WriteFile("E:\\Github\\Nymph\\html_go\\tmp\\basic.html", d1, 0644)
-    check(err)
+	createfile();
+	
     
 
 }
